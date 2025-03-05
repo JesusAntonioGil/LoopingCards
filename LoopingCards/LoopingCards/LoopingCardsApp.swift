e713dbb446e0b821c935fdb9dbd706ec3084cbf5
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-03-05 10:49:16 +0100

    Create project

---
+++ LoopingCards/LoopingCards/LoopingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopingCardsApp.swift
+//  LoopingCards
+//
+//  Created by Jesus Antonio Gil on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LoopingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
